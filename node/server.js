f530c9b7893c3a264cef0d6ac667d20c8b37bece
J.W. Clark <jwclark@rockhursths.edu>
2017-01-13 05:04:58 -0600

    Add the home route and a Mongo connection

--- node/server.js
+++ node/server.js
@@ -2,16 +2,30 @@ var http = require('http');
 var Mongo = require('mongodb').MongoClient;
 var express = require('express');
 
-var mongo_url = 'mongodb://localhost:27017/myapp';
+var mongoUrl = 'mongodb://localhost:27017/myapp';
+
+Mongo.connect(mongoUrl, function(err, db) {
+
+    if (err) {
+        log('MongoDB connection error');
+    } else {
+        log('Connected to MongoDB');
+    }
+
+    Mongo.ops = {};
+
+});
 
 function log(msg) {
-  console.log(msg);
+    console.log(msg);
 }
 
-Mongo.connect(mongo_url, function(err, db) {
-  if(err) {
-    log('MongoDB connection error');
-  } else {
-    log('Connected to MongoDB');
-  }
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('hello');
+});
+
+app.listen(3000, function() {
+    log('listening on port 3000');
 });
