be9227485ee48cb30816209d0b3bc5f1a4cc54f0
J.W. Clark <jwclark@rockhursths.edu>
2017-01-19 11:22:14 -0600

    Add some post routes for G signin

--- main.js
+++ main.js
@@ -3,65 +3,48 @@ if (window.location.hostname === '127.0.0.1') {
   window.location = 'http://localhost:1898';
 }
 
-var auth2;
-
-function appStart() {
-  gapi.load('auth2', initSigninV2);
-}
-
-function initSigninV2() {
-  auth2 = gapi.auth2.getAuthInstance();
-  auth2.isSignedIn.listen(signinChanged);
-  auth2.currentUser.listen(userChanged);
-  if (auth2.isSignedIn.get() == true) {
-    auth2.signIn();
-  }
-}
-
-function signinChanged(isSignedIn) {
-  console.log('signinChanged() = ' + isSignedIn);
-  if (isSignedIn) {
-    console.log('the user must be signed in to print this');
-    var googleUser = auth2.currentUser.get();
-    var authResponse = googleUser.getAuthResponse();
-    var profile = googleUser.getBasicProfile();
-    
-    $('#email').html('<p>' + profile.getEmail() + '</p>');
-    $('#photo').html('<img src="' + profile.getImageUrl() + '">');
-    
-    // some other properties
-    console.log(profile.getId());
-    console.log(profile.getName());
-    console.log(profile.getGivenName());
-    console.log(profile.getFamilyName());
-    console.log(profile.getImageUrl());
-    console.log(profile.getEmail());
-    console.log(googleUser.getHostedDomain());
-    console.log(authResponse.id_token);
-    console.log(authResponse.expires_at);
-
-  } else {
-    console.log('the user must not be signed in if this is printing');
-  }
-}
-
-function userChanged() {
-  console.log('userChanged()');
-}
-
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
+  var authResponse = googleUser.getAuthResponse();
+  
   $('.g-signin2').hide();
+  $('#email').html('<p>' + profile.getEmail() + '</p>');
+  $('#photo').html('<img src="' + profile.getImageUrl() + '">');
+
+  // properties from google
+  console.log(profile.getId());
+  console.log(profile.getName());
+  console.log(profile.getGivenName());
+  console.log(profile.getFamilyName());
+  console.log(profile.getImageUrl());
+  console.log(profile.getEmail());
+  console.log(googleUser.getHostedDomain());
+  console.log(authResponse.id_token);
+  console.log(authResponse.expires_at);
+  
+  var json = {
+    id: profile.getId(),
+    name: profile.getName(),
+    givenName: profile.getGivenName()
+  };
+  
+  console.log(JSON.stringify(json));
+  
+  $.post('http://10.10.90.58:3000/signin', json, function(data) {
+    $('#post-status').text(data);
+  });
 }
 
 function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
-  });
+  gapi.auth2.getAuthInstance().signOut();
   $('.g-signin2').show();
+  $('#email').html('');
+  $('#photo').html('');
+}
+
+function disconnect() {
+  gapi.auth2.getAuthInstance().disconnect();
+  $('.g-signin2').show();
+  $('#email').html('');
+  $('#photo').html('');
 }
--- node/server.js
+++ node/server.js
@@ -1,17 +1,87 @@
 var http = require('http');
 var Mongo = require('mongodb').MongoClient;
 var express = require('express');
+var bodyParser = require('body-parser');
 
-var mongo_url = 'mongodb://localhost:27017/myapp';
+var mongo_url = 'mongodb://localhost:27017/apcsp';
 
-function log(msg) {
-  console.log(msg);
-}
+// custom logging
+var log = function(msg, obj) {
+    console.log('\n');
+    if(obj) {
+        try {
+            console.log(msg + JSON.stringify(obj));
+        } catch(err) {
+            var simpleObject = {};
+            for (var prop in obj ){
+                if (!obj.hasOwnProperty(prop)){
+                    continue;
+                }
+                if (typeof(obj[prop]) == 'object'){
+                    continue;
+                }
+                if (typeof(obj[prop]) == 'function'){
+                    continue;
+                }
+                simpleObject[prop] = obj[prop];
+            }
+            console.log('circular-' + msg + JSON.stringify(simpleObject)); // returns cleaned up JSON
+        }        
+    } else {
+        console.log(msg);
+    }
+};
 
-Mongo.connect(mongo_url, function(err, db) {
-  if(err) {
+// static MongoDB operations
+Mongo.connect(mongo_url, function (err, db) {
+
+  if (err) {
     log('MongoDB connection error');
   } else {
     log('Connected to MongoDB');
+    
+    Mongo.ops = {};
+    
+    Mongo.ops.insert = function (collection, json, callback) {
+      var col = db.collection(collection);
+      col.insert(json, function (err, result) {
+        if (err) {
+          log('insert error: ' + err);
+        }
+        else {
+          log('insert success: ' + collection + ' = ', json);
+        }
+        if (callback) callback(err, result);
+      });
+    };
   }
 });
+
+// http server
+var app = express();
+
+// body parsing ensures req.body property
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.send('hello');
+});
+
+app.post('/signin', function (req, res) {
+  log('req.body = ', req);
+  Mongo.ops.insert('signin', req.body);
+  res.status(201).send('ok');
+});
+
+// listen on port 3000
+app.listen(3000);
+
+log('listening on port 3000');
